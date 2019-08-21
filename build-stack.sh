74ab83ec0eed5aa72e4f305e3f22e25607fc4e8a
yazzou <ytouaibia1@gmail.com>
2019-08-21 11:22:03 +0200

    Update build-stack.sh

--- build-stack.sh
+++ build-stack.sh
@@ -4,7 +4,7 @@
 DBPASSWORD="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c32)"
 WPADMINPASSWORD="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c8)"
 
-echo "What is your email address? (needed for SSL cert and wp-admin setup)"
+echo "Adresse mail à utiliser ? (needed for SSL cert and wp-admin setup)"
 read ADMINEMAIL
 
 echo "Which URL should I configure this build for? (e.g. www.wpintense.com)"
@@ -13,36 +13,14 @@ read SITEURL
 echo "$DBPASSWORD" > dbpassword.txt
 echo "$WPADMINPASSWORD" > wpadminpassword.txt
 
-export DEBIAN_FRONTEND=noninteractive
-wget -c https://dev.mysql.com/get/mysql-apt-config_0.8.10-1_all.deb
-dpkg -i mysql-apt-config_0.8.10-1_all.deb
-apt-get update -y
-apt-get upgrade -y
-apt-get install debconf-utils -y
-echo "mysql-community-server  mysql-server/default-auth-override      select  Use Strong Password Encryption (RECOMMENDED)" | debconf-set-selections
-apt-get install mysql-server -y # accept all defaults
-apt-get install php7.2 -y
-apt-get purge apache2 -y
-apt-get install nginx -y
-apt-get install -y tmux curl wget php7.2-fpm php7.2-cli php7.2-curl php7.2-gd php7.2-intl 
-apt-get install -y php7.2-mysql php7.2-mbstring php7.2-zip php7.2-xml unzip
-apt-get install -y redis
-apt-get install -y fail2ban
-
-#redis config
-echo "maxmemory 300mb" >> /etc/redis/redis.conf
-echo "maxmemory-policy allkeys-lru" >> /etc/redis/redis.conf
-
-sed -i 's/^save /#save /gi' /etc/redis/redis.conf
 
 #nginx config
-git clone https://github.com/dhilditch/wpintense-rocket-stack-ubuntu18-wordpress /root/rsconfig
-cp /root/rsconfig/nginx/* /etc/nginx/ -R
+#git clone https://github.com/dhilditch/wpintense-rocket-stack-ubuntu18-wordpress /root/rsconfig
+#cp /root/rsconfig/nginx/* /etc/nginx/ -R
+
 ln -s /etc/nginx/sites-available/rocketstack.conf /etc/nginx/sites-enabled/
-rm /etc/nginx/sites-enabled/default
-rm /etc/nginx/sites-available/default
-mkdir /var/www/cache
-chown www-data:www-data /var/www/cache/
+mkdir /var/www/SITEURL/cache
+chown www-data:www-data /var/www/SITEURL/cache/
 #change server_name variable to domain name of website inc www
 sed -i "s/server_name _;/server_name $SITEURL;/gi" /etc/nginx/sites-available/rocketstack.conf
 
