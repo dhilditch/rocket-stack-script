69b3fb65422d7d2987641cf1de4d84c7544a444d
yazzou <ytouaibia1@gmail.com>
2019-08-21 15:13:34 +0200

    Update build-stack.sh

--- build-stack.sh
+++ build-stack.sh
@@ -17,9 +17,7 @@ echo "$DBPASSWORD" > dbpassword.txt
 echo "$WPADMINPASSWORD" > wpadminpassword.txt
 
 
-#nginx config
-#git clone https://github.com/dhilditch/wpintense-rocket-stack-ubuntu18-wordpress /root/rsconfig
-#cp /root/rsconfig/nginx/* /etc/nginx/ -R
+
 
 cp /etc/nginx/sites-available/rocketstack.conf /etc/nginx/sites-available/$SITEURL.conf
 ln -s /etc/nginx/sites-available/$SITEURL.conf /etc/nginx/sites-enabled/
@@ -42,25 +40,19 @@ service nginx restart
 
 #wordpress installation
 
-mkdir /var/www/rocketstack
-chown www-data:www-data /var/www/rocketstack
-cd /var/www/rocketstack
+mkdir /var/www/$SITEURL
+chown www-data:www-data /var/www/$SITEURL
+cd /var/www/$SITEURL
 wp core download --allow-root
 chown www-data:www-data * -R
-wp core config --dbhost=localhost --dbname=rocketstack --dbuser=rs --dbpass=$DBPASSWORD --allow-root
+wp core config --dbhost=localhost --dbname=$DBUSER --dbuser=$DBUSER --dbpass=$DBPASSWORD --allow-root
 chmod 644 wp-config.php
-wp core install --url="https://${SITEURL}" --title="Rocket Stack Installation by www.wpintense.com" --admin_name=rsadmin --admin_password=$WPADMINPASSWORD --admin_email=$ADMINEMAIL  --allow-root
-chown www-data:www-data /var/www/rocketstack -R
-
-#wget https://wordpress.org/latest.zip -P /var/www/
-#unzip /var/www/latest.zip -d /var/www/
-#mv /var/www/wordpress /var/www/rocketstack
-#chown www-data:www-data /var/www/rocketstack -R
-#rm /var/www/latest.zip
+wp core install --url="https://${SITEURL}" --title="changer le titre du site" --admin_name=yazzou --admin_password=$WPADMINPASSWORD --admin_email=$ADMINEMAIL  --allow-root
+chown www-data:www-data /var/www/$SITEURL -R
 
 #mysql_secure_installation # choose y for everything and enter a secure root password
 echo "You can now log in through https://${SITEURL}/wp-login.php"
-echo "Username: rsadmin";
+echo "Username: yazzou";
 echo "Password: $WPADMINPASSWORD";
 
 
